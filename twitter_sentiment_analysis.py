3261e5d5ad486e8bcf8baa455bea9ff63de23fca
TOlutayo <olutayo@bu.edu>
2019-09-21 15:12:48 -0400

    Tweet Retrieval
    This is the main python code for the tweet retrieval module.

--- twitter_sentiment_analysis.py
+++ twitter_sentiment_analysis.py
@@ -1,8 +1,8 @@
 import tweepy
-consumer_key = '7aybdIZTDuBFiq1moEyKhRYGe'
-consumer_secret ='LRDbWiaTPnfre3v4PRlrc1cSWcXuP8XTRH5yihVATkgB3CHyTE'
-access_token = '1171884577087873024-JOazU8fgLKOIoq8PAwuMbVhEciRDh3'
-access_token_secret = 'iStiWwZNtjvBXOJX1PXRm6vrmrIJ8aCQOQXgD3QJaWiOm'
+consumer_key = ''
+consumer_secret =''
+access_token = ''
+access_token_secret = ''
 
 
 
