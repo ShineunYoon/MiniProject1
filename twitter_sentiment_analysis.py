8927d66fd2014a3cd7ca831721e7f9d4db590330
ShineunYoon <syoon19@hawk.iit.edu>
2019-09-29 19:56:31 -0400

    Removed URLs in Tweet.text

--- twitter_sentiment_analysis.py
+++ twitter_sentiment_analysis.py
@@ -1,9 +1,7 @@
 import tweepy
+import re
 
-consumer_key = 'Y0nziezvpeBLvENio4hvF5Rwx'
-consumer_secret ='KdxeyJGJEh4pLq6ebPEp3yuCBf522KLqbAvPsZOHlYgrw1o0xR'
-access_token = '1171884577087873024-9O04MqVWQOUGduSKjHBOf6p6vJLXdQ'
-access_token_secret = 'J4uQhozPVPU5mkp1quABrQ9FbEuiSegSp6qLmilu6o5zF'
+#
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -13,6 +11,10 @@ def get_tweets(*args):
    file=open('./tweet.text', 'w+')
    tweets = tweepy.Cursor(api.search,q=args[0],lang='en').items(args[1])
    for tweet in tweets:
-      file.write(tweet.text)
+      text = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', tweet.text)
+      file.write(text)
       file.write('\n')
    file.close()
+
+# if __name__ == "__main__":
+#     get_tweets("nike",10)
