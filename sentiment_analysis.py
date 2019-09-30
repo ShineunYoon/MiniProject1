2555872f326f9c7107df8599aacce78799acd958
ShineunYoon <syoon19@hawk.iit.edu>
2019-09-29 23:21:08 -0400

    Updated Scorer

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,9 +2,7 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
-
 def analyze(*args):
-    accumulative = 0
 
     client = language.LanguageServiceClient()
 
@@ -12,28 +10,11 @@ def analyze(*args):
         content = tweets.read()
 
     document = types.Document(content=content, type=enums.Document.Type.PLAIN_TEXT)
-
     analysis = client.analyze_sentiment(document=document)
+
+    # Overall Sentiment Analysis
     score = analysis.document_sentiment.score
     magnitude = analysis.document_sentiment.magnitude
+    standardized_score = (score / magnitude) * 100
 
-    for index, line in enumerate(analysis.sentences):
-        line_score = line.sentiment.score
-        accumulative = accumulative + line_score
-
-    emotion = (accumulative / magnitude) * 100
-
-    print('Overall Sentiment: score of {} with magnitude of {}'.format(accumulative, magnitude))
-    comment = scorer(emotion);
-    return comment
-
-
-def scorer(emotion):
-    if emotion < 20:
-        return "Terrible"
-    elif 20 <= emotion < 50:
-        return "Think once again"
-    elif 50 <= emotion < 80:
-        return "Sounds good"
-    else:
-        return "Go for it!"
+    return standardized_score
